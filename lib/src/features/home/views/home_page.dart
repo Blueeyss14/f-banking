448f6dbf1c107d9bae7ac3e9397bf1c9f2bd0c9d
unknown <delkanoberutu8@gmail.com>
2025-04-08 23:38:47 +0700

    update dulu lah

--- lib/src/features/home/views/home_page.dart
+++ lib/src/features/home/views/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:f_banking/src/shared/widgets/menu_icon_animation.dart';
 import 'package:f_banking/src/shared/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/flipcard/flip_card.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:flutter_flip_card/modal/flip_side.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 bool isClicked = false;
+FlipCardController flipCardController = FlipCardController();
 
 class _HomePageState extends State<HomePage> {
   @override
@@ -28,9 +32,30 @@ class _HomePageState extends State<HomePage> {
                           ? MediaQuery.of(context).size.width / 8 + 16
                           : 0,
                   height: double.infinity,
-                  duration: Duration(milliseconds: 300),
+                  duration: Duration(milliseconds: 200),
                 ),
-                Flexible(child: Container(color: Colors.green)),
+                Flexible(
+                  child: Container(
+                    alignment: Alignment.center,
+                    // width: double.infinity,
+                    // height: double.infinity,
+                    child: Column(
+                      children: [
+                        FlipCard(
+                          frontWidget: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 200,
+                            color: Colors.green,
+                            child: Text("data"),
+                          ),
+                          backWidget: Container(),
+                          controller: flipCardController,
+                          rotateSide: RotateSide.bottom,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
             Column(
--- lib/src/shared/widgets/menu_icon_animation.dart
+++ lib/src/shared/widgets/menu_icon_animation.dart
@@ -2,40 +2,34 @@ import 'package:f_banking/src/shared/components/custom_dot_menu.dart';
 import 'package:flutter/material.dart';
 
 Widget buildMenuIconAnimation(bool isClicked, VoidCallback onTap) {
-  return StatefulBuilder(
-    builder: (context, setState) {
-      return GestureDetector(
-        onTap: onTap,
-        child: AnimatedContainer(
-          margin: const EdgeInsets.all(15),
-          alignment: Alignment.center,
-          height: isClicked ? 25 : 20,
-          width: 35,
-          color: Colors.transparent,
-          duration: Duration(milliseconds: 300),
-          child: Stack(
-            children: [
-              AnimatedAlign(
-                alignment:
-                    isClicked ? Alignment.bottomRight : Alignment.centerLeft,
-                duration: Duration(milliseconds: 300),
-                child: CustomDotMenu(),
-              ),
-              AnimatedAlign(
-                alignment: isClicked ? Alignment.topCenter : Alignment.center,
-                duration: Duration(milliseconds: 300),
-                child: CustomDotMenu(),
-              ),
-              AnimatedAlign(
-                alignment:
-                    isClicked ? Alignment.bottomLeft : Alignment.centerRight,
-                duration: Duration(milliseconds: 300),
-                child: CustomDotMenu(),
-              ),
-            ],
+  return GestureDetector(
+    onTap: onTap,
+    child: AnimatedContainer(
+      margin: const EdgeInsets.all(15),
+      alignment: Alignment.center,
+      height: isClicked ? 25 : 20,
+      width: 35,
+      color: Colors.transparent,
+      duration: Duration(milliseconds: 300),
+      child: Stack(
+        children: [
+          AnimatedAlign(
+            alignment: isClicked ? Alignment.bottomRight : Alignment.centerLeft,
+            duration: Duration(milliseconds: 300),
+            child: CustomDotMenu(),
           ),
-        ),
-      );
-    },
+          AnimatedAlign(
+            alignment: isClicked ? Alignment.topCenter : Alignment.center,
+            duration: Duration(milliseconds: 300),
+            child: CustomDotMenu(),
+          ),
+          AnimatedAlign(
+            alignment: isClicked ? Alignment.bottomLeft : Alignment.centerRight,
+            duration: Duration(milliseconds: 300),
+            child: CustomDotMenu(),
+          ),
+        ],
+      ),
+    ),
   );
 }
