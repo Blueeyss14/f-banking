3409777ca3d7f702901f5dc82142ddcdf770dfd5
unknown <delkanoberutu8@gmail.com>
2025-04-12 03:45:52 +0700

    fix transaction activity

--- lib/src/features/home/views/pages/transaction_page.dart
+++ lib/src/features/home/views/pages/transaction_page.dart
@@ -1,4 +1,3 @@
-import 'package:f_banking/src/shared/components/activity.dart';
 import 'package:f_banking/src/shared/components/transaction_activity.dart';
 import 'package:f_banking/src/shared/style.dart';
 import 'package:flutter/material.dart';
@@ -9,28 +8,35 @@ class TransactionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 100),
-          Text(
-            "Payment Activity",
-            style: TextStyle(
-              color: white,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 120),
+            Text(
+              "Payment Activity",
+              style: TextStyle(
+                color: white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          TransactionActivity(
-            itemCount: 10,
-            titles: ["sgsg", "sgsg", "sgsg", "sgsg"],
-          ),
-          Container(
-            color: Colors.amber,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-        ],
+            const SizedBox(height: 20),
+            TransactionActivity(
+              margin: const EdgeInsets.only(bottom: 5),
+              width: MediaQuery.of(context).size.width - 100,
+              itemCount: 10,
+              titles: ["sdgfsdfsd", "sdfsdfsdfsdf"],
+              subTitles: [
+                "sdfsdfhsufhsdkjh shdfk sjhdfshdfjk jshfjsdfhkjhkjh",
+                "sdfsdfsfs",
+                "dsfsdgsdgs",
+              ],
+            ),
+            Container(height: MediaQuery.of(context).size.width),
+          ],
+        ),
       ),
     );
   }
--- lib/src/shared/components/transaction_activity.dart
+++ lib/src/shared/components/transaction_activity.dart
@@ -8,6 +8,9 @@ class TransactionActivity extends StatelessWidget {
   final List<String?>? subTitles;
   final List<Widget?>? images;
   final int? itemCount;
+  final double? width;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
 
   const TransactionActivity({
     super.key,
@@ -15,23 +18,29 @@ class TransactionActivity extends StatelessWidget {
     this.subTitles,
     this.images,
     this.itemCount,
+    this.width,
+    this.padding,
+    this.margin,
   });
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
+      scrollDirection: Axis.vertical,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: List.generate(
           itemCount ?? 0,
           (index) => Item(
+            width: width,
             gradientColor: LinearGradient(
               colors: [const Color(0xFF2A3D55).withAlpha(50), darkBlue2],
               begin: Alignment.bottomLeft,
               end: Alignment.topRight,
             ),
-            margin: const EdgeInsets.only(right: 10),
-            padding: const EdgeInsets.all(10),
+            margin: margin ?? const EdgeInsets.only(right: 10),
+            padding: padding ?? const EdgeInsets.all(10),
             child: Row(
               children: [
                 Container(
@@ -47,27 +56,33 @@ class TransactionActivity extends StatelessWidget {
                           ? images![index]
                           : const SizedBox(),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AutoSizeText(
-                      titles != null && index < titles!.length
-                          ? titles![index] ?? "title"
-                          : "title",
-                      style: TextStyle(
-                        color: white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AutoSizeText(
+                        titles != null && index < titles!.length
+                            ? titles![index] ?? "title"
+                            : "title",
+                        style: TextStyle(
+                          color: white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        maxLines: 1,
                       ),
-                      maxLines: 1,
-                    ),
-                    AutoSizeText(
-                      subTitles != null && index < subTitles!.length
-                          ? subTitles![index] ?? "Sub Title"
-                          : "Sub Title",
-                      style: TextStyle(color: white),
-                    ),
-                  ],
+                      AutoSizeText(
+                        subTitles != null && index < subTitles!.length
+                            ? subTitles![index] ?? "Sub Title"
+                            : "Sub Title",
+                        style: TextStyle(
+                          color: white,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
